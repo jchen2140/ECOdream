4a8a1526892b9a5ae3c14f9824a1068c6d18318f
Jessie Chen <jessch.2140@gmail.com>
2021-08-04 12:16:43 -0700

    Added age range and directories

---
+++ ECOdream/AgeRangeViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AgeRangeViewController.swift
+//  ECOdream
+//
+//  Created by Jessie Chen on 8/4/21.
+//
+
+import UIKit
+
+class AgeRangeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
