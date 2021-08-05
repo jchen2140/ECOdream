658af2d85b09853e5b1ec67f121734d87db85842
Elizabeth <eliza>
2021-08-05 10:58:41 -0700

    randomized tips

--- ECOdream/ChildFactViewController.swift
+++ ECOdream/ChildFactViewController.swift
@@ -8,6 +8,16 @@
 import UIKit
 
 class ChildFactViewController: UIViewController {
+    
+    
+    @IBOutlet weak var childTipLabel: UILabel!
+    @IBOutlet weak var childTipButton: UIButton!
+    
+    @IBAction func submitTipChild (_ sender: Any) {
+        var quickTips = ["Turn the lights off when you leave a room.", "Turn the faucet off when you brush your teeth.", "Learn what products can be recycled.", "Reduce, Re-use, Recycle!", "Avoid single use plastics", "Shop locally, shop organically.", "Plant pollinator-friendly plants that are native to your region."]
+        var randomInt = Int.random(in: 0..<7)
+        childTipLabel.text = quickTips[randomInt]
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
